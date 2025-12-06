04622054509e92f28be306c6f7618dd318da530e
intelligencecore <07wood.temblor@icloud.com>
2025-12-05 21:30:38 -0500

    Initial Commit

---
+++ swiftCourseraProject/logoView.swift
@@ -0,0 +1,27 @@
+//
+//  buttonView.swift
+//  swiftCourseraProject
+//
+//  Created by Richie on 8/20/25.
+//
+
+
+import SwiftUI
+
+struct logoView: View {
+    var body: some View {
+        
+        
+        Image("AppIcon-dark")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 100, height: 100)
+    }
+}
+
+
+
+
+#Preview {
+    logoView()
+}
---
+++ swiftCourseraProject/swiftCourseraProjectApp.swift
@@ -0,0 +1,25 @@
+//
+//  swiftCourseraProjectApp.swift
+//  swiftCourseraProject
+//
+//  Created by Richie on 8/13/25.
+//
+
+
+
+
+import SwiftUI
+
+
+// the @main is the entry point of a swift app
+@main
+// there is always a main strucure in a app
+struct basicViews: App {
+  var body: some Scene {
+    WindowGroup {
+        ContentView()
+    }
+  }
+}
+
+
